2e1b47f903df8cdffa1207b064c59c16d9262a1c
littlecobber <81452190+littlecobber@users.noreply.github.com>
2023-01-29 08:52:04 -0500

    Update and rename Final-1.py to part2.py

---
+++ part2.py
@@ -0,0 +1,26 @@
+# part2 
+
+def run_part2(image):
+
+    #loade the quantum classifier circuit
+    classifier=qiskit.QuantumCircuit.from_pickle_file('quantum_classifier.pickle')
+    
+    #encode image into circuit
+    circuit=encode(image)
+    
+    #append with classifier circuit
+    nq1 = circuit.width()
+    nq2 = classifier.width()
+    nq = max(nq1, nq2)
+    qc = qiskit.QuantumCircuit(nq)
+    qc.append(circuit.to_instruction(), list(range(nq1)))
+    qc.append(classifier.to_instruction(), list(range(nq2)))
+    
+    #simulate circuit
+    histogram=simulate(qc)
+        
+    #convert histogram to category
+    label=histogram_to_category(histogram)
+        
+    return circuit,label
+#score
